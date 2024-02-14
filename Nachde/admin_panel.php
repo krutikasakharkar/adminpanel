260e7eeaea78b5a7543f28641b50d93e07b0617f
krutikasakharkar <138954542+krutikasakharkar@users.noreply.github.com>
2024-02-14 16:03:41 +0530

    Add files via upload

---
+++ Nachde/admin_panel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admission Admin panel</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Admission Admin panel</h2>
+        <div class="actions">
+            <button onclick="location.href='add_admission.php'">Add New Admission</button>
+            <button onclick="location.href='view_admission.php'">View Admission</button>
+            <button onclick="location.href='remove_admission.php'">Remove Admission</button>
+            <button onclick="location.href='update_admission.php'">Update Admission</button>
+            
+        </div>
+    </div>
+</body>
+</html>
